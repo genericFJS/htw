7effaa0914bf2deb4aea33073a2d3f5b6e57b2fb
FJS <falkjonatan.strube@gmail.com>
2015-10-28 17:34:15 +0100

    initial

---
+++ Vorlesungen/Programmierung 1/Praktikum/prak1/ascii.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <ctype.h>
+
+void main(){
+	int i, j;
+	for (i=0; i<4; i++)
+		printf("| dec hex Char")
+	printf('l\n');
+	for (i=0; i<128/4; j+= 128/4){
+		printf ("\n| ");
+		for (j=0; j<128; j+= 128/4){
+			printf("%3d %2X ",j,j);
+			if (isgraph(j))
+				printf(" %c | ", j);
+			else 
+				printf (" . | ");
+		}
+	}
+}
