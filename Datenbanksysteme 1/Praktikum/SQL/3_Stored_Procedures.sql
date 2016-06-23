5256879ddac0c6fd6a4ca3fc38080e31f461615c
genericFJS <freejailsymphonic@yahoo.de>
2016-06-23 12:28:33 +0200

    DBS Praktikum fertig :-)
    Former-commit-id: ff8957196ba3285475c5399d64f4c217fec046ec [formerly 2964bc8db2127310ee9f4393a053cc8af6f8337b]
    Former-commit-id: 612dc7648bde69e5b8279efd04196336c2c7012d

--- Datenbanksysteme 1/Praktikum/SQL/3_Stored_Procedures.sql
+++ Datenbanksysteme 1/Praktikum/SQL/3_Stored_Procedures.sql
@@ -24,23 +24,27 @@ DROP PROCEDURE Display_MPB
 
 --==================================================--
 -- 3.3
-SELECT * INTO Mita FROM Mitarbeiter
-
-CREATE PROCEDURE NewYear AS
+CREATE PROCEDURE NewYear (@years int) AS
 BEGIN
-	UPDATE Mita SET Gebdat = DATEADD(YEAR, -1, GebDat)
+	WHILE EXISTS (select MitID from Mita)
+	BEGIN
+		IF @years < 1
+			BREAK
+		UPDATE Mita SET Gebdat = DATEADD(YEAR, -1, GebDat)
 
-	DELETE FROM Mita
-	WHERE DATEDIFF(yyyy, Gebdat, GETDATE()) >= 60
+		DELETE FROM Mita
+		WHERE DATEDIFF(yyyy, Gebdat, GETDATE()) >= 60
+
+		SELECT @years = @years - 1
+
+		WAITFOR DELAY '00:00:01';
+		SELECT MitID, DATEDIFF(yyyy, Gebdat, GETDATE()) AS MitAlter FROM Mita
+	END
 END
 	
-WHILE EXISTS (select MitID from Mita)
-BEGIN
-	EXEC NewYear
+SELECT * INTO Mita FROM Mitarbeiter
 
-	WAITFOR DELAY '00:00:01';
-	SELECT MitID, DATEDIFF(yyyy, Gebdat, GETDATE()) AS MitAlter FROM Mita
-END
+EXEC NewYear 5
 
 DROP TABLE Mita
 
@@ -57,33 +61,109 @@ DECLARE myCursor CURSOR FOR
 
 OPEN myCursor
 
+FETCH myCursor INTO @myID
 WHILE @@fetch_status != -1
 BEGIN
-	FETCH myCursor INTO @myID
 	print @myID
+	FETCH myCursor INTO @myID
 END
 
 CLOSE myCursor
 DEALLOCATE myCursor
 
 -- a
+SELECT * FROM Mitarbeiter
+
 CREATE PROCEDURE myCP (@MitID char(3)) AS
 BEGIN
+	--DECLARE @MitID CHAR(3) = 102
+
+	DECLARE @myID CHAR(15)
 	DECLARE @myName CHAR(20)
 	DECLARE @myVorname CHAR(20)
 	DECLARE @myBeruf CHAR(15)
+	DECLARE @myBeschr CHAR(30)
+	DECLARE @myPlan FLOAT
+	DECLARE @myIst FLOAT
+	DECLARE @myDiff FLOAT
 
 	DECLARE myCursor CURSOR FOR
-		SELECT Nachname, Vorname, Beruf FROM Mitarbeiter
+		SELECT m.MitId, m.Nachname, m.Vorname, m.Beruf, p.ProBeschreibung, z.PlanAnteil, z.IstAnteil, z.PlanAnteil - z.IstAnteil AS PIdiff
+		FROM (
+			SELECT * 
+			FROM Mitarbeiter 
+			WHERE MitId=@MitID) m
+		LEFT JOIN Zuordnung z ON z.MitID = m.MitID
+		LEFT JOIN Projekt p ON p.ProNr = z.ProNr
 
+	-- a
 	OPEN myCursor
 
-	WHILE @@fetch_status != -1
+	PRINT '1. Ausgabeteil: Mitarbeiter'
+
+	FETCH myCursor INTO @myID, @myName, @myVorname, @myBeruf, @myBeschr, @myPlan, @myIst, @myDiff
+	IF @@fetch_status != -1
 	BEGIN
-		FETCH myCursor INTO @myName
-		print @myName + @myVorname + @myBeruf
+		PRINT @myName + @myVorname + @myBeruf
+	END
+	ELSE
+	BEGIN
+		PRINT 'Mitarbeiter ' + @MitID + ' nicht gefunden.'
 	END
 
 	CLOSE myCursor
+
+	-- b
+	OPEN myCursor
+	
+	PRINT '2. Ausgabeteil: Projekt(e)'
+
+	FETCH myCursor INTO @myID, @myName, @myVorname, @myBeruf, @myBeschr, @myPlan, @myIst, @myDiff
+	WHILE @@fetch_status != -1
+	BEGIN
+		PRINT ISNULL(@myBeschr,'(keine Beschreibung)') + ' Plan-Anteil: ' + CONVERT(CHAR(10),@myPlan) + ' Ist-Anteil: ' + CONVERT(CHAR(10),@myIst) + ' Differenz: ' + CONVERT(CHAR(10),@myDiff)
+		FETCH myCursor INTO @myID, @myName, @myVorname, @myBeruf, @myBeschr, @myPlan, @myIst, @myDiff
+	END
+
+	CLOSE myCursor
+
+	-- c
+	OPEN myCursor
+	
+	PRINT '3. Ausgabeteil: Auslastung'
+
+	DECLARE @myPlanSum FLOAT = 0
+	DECLARE @myIstSum FLOAT = 0
+	DECLARE @myKulm FLOAT = 0
+
+	FETCH myCursor INTO @myID, @myName, @myVorname, @myBeruf, @myBeschr, @myPlan, @myIst, @myDiff
+	WHILE @@fetch_status != -1
+	BEGIN
+		SELECT @myPlanSum = @myPlanSum + @myPlan
+		SELECT @myIstSum = @myIstSum + @myIst
+		SELECT @myKulm = @myPlanSum - @myIstSum
+		FETCH myCursor INTO @myID, @myName, @myVorname, @myBeruf, @myBeschr, @myPlan, @myIst, @myDiff
+	END
+	IF @myPlanSum > 1
+		PRINT 'Mitarbeiter ' + RTRIM(@myName) + ' ist überfordert!'
+	ELSE
+	BEGIN
+		IF @myKulm = 0
+			PRINT 'Mitarbeiter ' + RTRIM(@myName) + ' ist optimal ausgelastet.'
+		IF @myKulm < 0
+			PRINT 'Mitarbeiter ' + RTRIM(@myName) + ' ist überlastet.'
+		IF @myKulm > 0
+			PRINT 'Mitarbeiter ' + RTRIM(@myName) + ' hat noch Reserven.'
+	END
+	--PRINT CONVERT(CHAR(10),@myPlanSum) + CONVERT(CHAR(10),@myIstSum) + CONVERT(CHAR(10),@myKulm)
+
+	CLOSE myCursor
+
 	DEALLOCATE myCursor
 END
+
+EXEC myCP 102
+
+DROP PROCEDURE myCP
+
+SELECT * From Mitarbeiter
