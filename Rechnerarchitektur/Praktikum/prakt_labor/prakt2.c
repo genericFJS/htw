020185b361871362a1ef890ea8207664a6a744ae
genericFJS <freejailsymphonic@yahoo.de>
2016-11-28 11:57:47 +0100

    :ledger: RA Praktikum Lauflicht
    Former-commit-id: 8a355d0328bca519a1e47e14f72b1de00130f87e [formerly f84963f1dbf70b104d7e1cbba1b533a43b9ead10]
    Former-commit-id: d336488b4376f8ac987c267a3b0a9fd094d3a46a

--- Rechnerarchitektur/Praktikum/prakt_labor/prakt2.c
+++ Rechnerarchitektur/Praktikum/prakt_labor/prakt2.c
@@ -2,17 +2,51 @@
 #include <avr/interrupt.h>
 //#include <stdint.h>
 
+int muster = 0x0F;
+int temp = 0;
+int right = 1; // 0: nach links, anderes: nach rechts
+
 // Interrupt Anweisung für Timer 0 Overflow
 ISR(TIMER0_OVF_vect){
-	PORTB = ~PORTB;
+	//PORTB = ~PORTB;
+	temp = PORTB;
+	if (right){	
+		PORTB >>= 1;
+		if (temp & (1<<0)){
+			PORTB |= (1<< 7);
+		}
+	} else {
+		PORTB <<= 1;
+		if (temp & (1<<7)){
+			PORTB |= 1;
+		}
+	}
+}
+
+// Interrupt Anweisung für Schalter 1
+ISR(INT2_vect){
+	if (right){
+		right = 0;
+	} else {
+		right = 1;
+	}
 }
 
 int main (void){
-	DDRB = 0xFF; // B komplett als Ausgabe definieren
+	DDRB = 0xFF;	// B komplett als Ausgabe definieren
+	DDRD = 0x00;	// D komplett als Eingang definieren
 	
 	// Timer: (alle 262ms)
 	TCCR0 = (1 << CS02) | (1 << CS01) |(1 << CS00);
 	TIMSK |= (1 << TOIE0);
+
+	// Tasten-Interrupt konfigurieren:
+	EICRA |= (1 << ISC21);
+	EICRA &= ~(1 << ISC20);
+	EIMSK = (1 << INT2);
+
+	
+	PORTB |= muster;
 
 	sei();	// Interrupts aktivieren
 
