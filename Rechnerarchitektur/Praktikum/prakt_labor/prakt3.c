96825415ce30e7d6a0938209bc60ee75965dd9db
genericFJS <freejailsymphonic@yahoo.de>
2016-11-28 12:59:05 +0100

    :ledger: RA Prakt 2 Abschluss
    Former-commit-id: 398ec534148fd51bee48c0c25b742df1f1ea5e7c [formerly 19ec4acaf4411fb711925718b060bebc95dede42]
    Former-commit-id: 4a75e51b2b120345dd2941eb5a6b7ed8c50ccdcf

--- Rechnerarchitektur/Praktikum/prakt_labor/prakt3.c
+++ Rechnerarchitektur/Praktikum/prakt_labor/prakt3.c
@@ -1,18 +1,26 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
-//#include <stdint.h>
 
 // Interrupt Anweisung für Timer 0 Overflow
 ISR(TIMER0_OVF_vect){
-	PORTB = ~PORTB;
+}
+
+// Interrupt Anweisung für Schalter 1
+ISR(INT2_vect){
 }
 
 int main (void){
-	DDRB = 0xFF;
+	DDRB = 0xFF;	// B komplett als Ausgabe definieren
+	DDRD = 0x00;	// D komplett als Eingang definieren
 	
 	// Timer: (alle 262ms)
 	TCCR0 = (1 << CS02) | (1 << CS01) |(1 << CS00);
 	TIMSK |= (1 << TOIE0);
+
+	// Tasten-Interrupt konfigurieren:
+	EICRA |= (1 << ISC21);
+	EICRA &= ~(1 << ISC20);
+	EIMSK = (1 << INT2);
 
 	sei();	// Interrupts aktivieren
 
