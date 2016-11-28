645dd0185b4665f572e7b96e71ccc27b377df86a
genericFJS <freejailsymphonic@yahoo.de>
2016-11-28 11:57:47 +0100

    :ledger: RA Praktikum Lauflicht
    Former-commit-id: 8a355d0328bca519a1e47e14f72b1de00130f87e [formerly f84963f1dbf70b104d7e1cbba1b533a43b9ead10]
    Former-commit-id: d336488b4376f8ac987c267a3b0a9fd094d3a46a
    Former-commit-id: 020185b361871362a1ef890ea8207664a6a744ae

---
+++ Rechnerarchitektur/Praktikum/prakt_labor/default/dep/prakt2.o.d
@@ -0,0 +1,34 @@
+prakt2.o: ../prakt2.c \
+  c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/avr/iom128.h \
+  c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h
+
+c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/avr/iom128.h:
+
+c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+c:/tools/avr/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h:
