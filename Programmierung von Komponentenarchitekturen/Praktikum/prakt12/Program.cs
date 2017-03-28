45a3400377611ff1d1e6ceef35bacc2cd5e320c5
genericFJS <freejailsymphonic@yahoo.de>
2017-03-28 14:19:33 +0200

    Create PKA Prakt 1+2
    Former-commit-id: 55abc9cd383ab78a29ada136155ad8c3195710ed
    Former-commit-id: 0a52e7a6641f594f3c72e9050a65bed646d1380d

---
+++ Programmierung von Komponentenarchitekturen/Praktikum/prakt12/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktikum1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World");
+            // Aufgabe 1
+            var a = 2L;
+            var b = 1.23456f;
+            var c = 0302;
+            var d = 100UL;
+            var e = .1e-5;
+            var f = 0x10;
+            var g = 0xFL;
+            Console.WriteLine("a: " + a + " (" + a.GetType() + ")");
+            Console.WriteLine("b: " + b + " (" + b.GetType() + ")");
+            Console.WriteLine("c: " + c + " (" + c.GetType() + ")");
+            Console.WriteLine("d: " + d + " (" + d.GetType() + ")");
+            Console.WriteLine("e: " + e + " (" + e.GetType() + ")");
+            Console.WriteLine("f: " + f + " (" + f.GetType() + ")");
+            Console.WriteLine("g: " + g + " (" + g.GetType() + ")");
+
+            // Aufgabe 2
+            int n, i; // nicht doppelt int
+            double sideLength;  // kein Leerzeichen in Variablenname
+            double slow_down = .1e-4;   // Zahlen nicht in Anführungszeichen
+            //double[] min = [5]; // Klammer bei Deklaration falsch, Initialisierung des Arrays nicht min Array-Name
+            double voidV = 1.23;    // Variablenname darf kein Keyword sein
+
+            // Aufgabe 3
+            // n=15, n+=25
+            //  n1=40
+            // n%9
+            //  n2=4
+            // n=5, n++
+            //  n3=5 (erst Ausgabe, dann zusammenrechnen)
+            // n*n, ++n (vor Ausgabe zusammenrechnen)
+            //  n4=36
+            n = 15;
+            n += 25;
+            Console.WriteLine("{0}", n);
+            n %= 9;
+            Console.WriteLine("{0}", n);
+            n = 5;
+            Console.WriteLine("{0}", n++);
+            n *= n;
+            Console.WriteLine("{0}", ++n);
+
+            // Aufgabe 4
+            // (1): foo in class C in math
+            // (2): foo2 in class 2 in math
+            // (3): foo2 in class C in ns in math1
+            // (4): foo in class C in ??
+            // (5): foo in class C in ??
+
+            // Aufgabe 5
+            // a: Deklaration der Variable w (darf noch nicht existieren) vom Typ widget
+            // b: Deklaration der Funktion w mit Ausgabe widget (die null ist)
+            // c: Abkürzung für einen getter (geht auch für setter) für die Variable w bei der Deklaration
+            // d: Cast des Typs widget auf die Variable w
+            // e: Deklaration und Initialisierung der Variable w vom Typ widget mit Wert x
+            // f: implizie Delkaration der Variable w mit Wert x
+        }
+    }
+}
---
+++ Programmierung von Komponentenarchitekturen/Praktikum/prakt2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prakt2 {
+    class Program {
+        static void Main(string[] args) {
+            Console.OutputEncoding = Encoding.UTF8; // Eurozeichen usw.
+            CultureInfo cultureDE = new CultureInfo("de-DE");   // Kommasymbol
+            CultureInfo.DefaultThreadCurrentCulture= cultureDE;
+            Console.Write("Startkapital in €: ");
+            double start;
+            if (!double.TryParse(Console.ReadLine(), out start)) {
+                Console.WriteLine("Kein gültiges Startkapital!");
+                return;
+            }
+            Console.Write("Laufzeit in Jahren: ");
+            int years;
+            if (!int.TryParse(Console.ReadLine(), out years)) {
+                Console.WriteLine("Keine gültige Laufzeit!");
+                return;
+            }
+            Console.Write("Zinssatz: ");
+            float interest;
+            if (!float.TryParse(Console.ReadLine(), out interest)) {
+                Console.WriteLine("Kein gültiger Zinssatz!");
+                return;
+            }
+            double end = start;
+            for (int i = years; i > 0; i--) {
+                end *= interest+1;
+                Console.Write("Jahr " + i);
+                Console.Write(": {0:c2}\n",end);
+            }
+            Console.Write("Das Endkapital beträgt {0:c2}", start*Math.Pow(1+interest,years));
+            Console.Write(" bei einem Zinssatz von {0:P2}\n", interest);
+        }
+    }
+}
