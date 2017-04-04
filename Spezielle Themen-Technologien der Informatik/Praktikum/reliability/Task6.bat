0dab54c9214d4647cd4d1b7cc3844b8f9554a4ee
genericFJS <freejailsymphonic@yahoo.de>
2017-04-04 12:28:52 +0200

    Update STTI Prakt
    Former-commit-id: c0e1c84d853e69d523070afe5b37e30d8d4ce135
    Former-commit-id: 80e703c757d9ffa343a8d61620d8368a31b02152

---
+++ Spezielle Themen-Technologien der Informatik/Praktikum/reliability/Task6.bat
@@ -0,0 +1,5 @@
+cd D:\Apps\mksimulator\
+mm_solve.exe -m EXAMPLE.model -M -t 1000000
+mm_solve.exe -m EXAMPLE1.model -M -t 1000000
+mm_solve.exe -m Single.model -M -t 1000000
+pause
---
+++ Spezielle Themen-Technologien der Informatik/Praktikum/reliability/Task7.bat
@@ -0,0 +1,4 @@
+cd D:\Apps\mksimulator\
+mm_solve.exe -m TASK7.model -M -t 1000000
+mm_solve.exe -m TASK7r.model -M -t 1000000
+pause
