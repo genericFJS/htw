82c092815280616ba9b2b61e411dadeb585948ff
genericFJS <freejailsymphonic@yahoo.de>
2017-04-05 19:14:52 +0200

    Update BS2 Prakt
    Former-commit-id: 5336cd9bfe20cc1c3b68e0ee993a5cb709384d29
    Former-commit-id: 3cf3f52d3c6b832dd7a19beb791a298b63971189

--- Betriebssysteme 2/Praktikum/prakt1/task1.c
+++ Betriebssysteme 2/Praktikum/prakt1/task1.c
@@ -3,18 +3,42 @@
 #include<sys/types.h>
 
 main(){
+	// initialize file
+	FILE *f = fopen("zahl.dat", "w");
+	fprintf(f, "0");
+	fclose(f);
+	// create children
+	pid_t pid = getppid();
 	int forks = 5;
-	pid_t pid;
-	pid = fork();
-	forks--;
 	for (forks; forks>0; forks--){
 		if(pid!=0){
 			pid = fork();
 		}
 	}
 	if(pid!=0){
-		printf("Vater\n");
+		// wait for children
+		wait(NULL);
+		wait(NULL);
+		wait(NULL);
+		wait(NULL);
+		wait(NULL);
 	} else {
-		printf("Sohn\n");
+		// write to file
+		int j= 0;
+		while (j<200){
+			FILE *fs = fopen("zahl.dat","r+");
+			int i;
+			fscanf(fs,"%d",&i);
+			rewind(fs);
+			fprintf(fs,"%d",++i);
+			fclose(fs);
+			j++;
+		}
+		// print number
+		FILE *fo = fopen("zahl.dat", "r");
+		int k;
+		fscanf(fo,"%d",&k);
+		printf("%d\n",k);
+		exit(0);
 	}
 }
