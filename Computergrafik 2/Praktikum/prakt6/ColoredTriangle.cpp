5140a10b840fe05697e0fab0be719ac8318ed9c0
genericFJS <freejailsymphonic@yahoo.de>
2017-06-19 16:39:49 +0200

    Update CG2 Prakt

--- Computergrafik 2/Praktikum/prakt6/ColoredTriangle.cpp
+++ Computergrafik 2/Praktikum/prakt6/ColoredTriangle.cpp
@@ -4,10 +4,44 @@
 #include <osgDB/WriteFile>
 #include <osgDB/ReadFile>
 #include <osg/ShapeDrawable>
-#include <osg/MatrixTransform>
-#include <osg/PositionAttitudeTransform>
 
 USE_GRAPHICSWINDOW()
+
+// scene definition
+osg::ref_ptr < osg::Node > createTriangle() {
+
+	// node which stores the geometry
+	osg::ref_ptr < osg::Geometry > geo = new osg::Geometry;
+
+	// array of vertices
+	osg::ref_ptr < osg::Vec3Array > v = new osg::Vec3Array;
+	geo->setVertexArray(v.get());
+	v->push_back(osg::Vec3(0.0f, 0.0f, 0.0f));
+	v->push_back(osg::Vec3(1.0f, 0.0f, 0.0f));
+	v->push_back(osg::Vec3(0.0f, 0.0f, 1.0f));
+
+	// array of colors
+	osg::ref_ptr < osg::Vec4Array > c = new osg::Vec4Array;
+	geo->setColorArray(c.get());
+	geo->setColorBinding(osg::Geometry::BIND_PER_VERTEX);
+	c->push_back(osg::Vec4(1.0f, 0.0f, 0.0f, 1.0f));
+	c->push_back(osg::Vec4(0.0f, 1.0f, 0.0f, 1.0f));
+	c->push_back(osg::Vec4(0.0f, 0.0f, 1.0f, 1.0f));
+
+	// array of normals
+	osg::ref_ptr < osg::Vec3Array > n = new osg::Vec3Array;
+	geo->setNormalArray(n.get());
+	geo->setNormalBinding(osg::Geometry::BIND_OVERALL);
+	n->push_back(osg::Vec3(0.0f, -1.0f, 0.0f));
+
+	// set face
+	geo->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::TRIANGLES, 0, 3));
+
+	// attach the drawable to a geode node
+	osg::ref_ptr < osg::Geode > geode = new osg::Geode;
+	geode->addDrawable(geo.get());
+	return geode.get();
+}
 
 osg::ref_ptr <osg::Node> addPlane() {
 	osg::ref_ptr <osg::Geometry> geo = new osg::Geometry;
@@ -61,40 +95,19 @@ int main(int argc, char * argv[]) {
 
 	// create an empty root node
 	osg::ref_ptr <osg::Group > root = new osg::Group;
-	// Gruppe für topologie mit Tranfsormierung:
-	// osg::ref_ptr <osg::Group > lollipop = new osg::Group;
-	// Gruppe mit Transformierung:
-	osg::ref_ptr<osg::MatrixTransform> lollipop = new osg::MatrixTransform;
-	osg::Matrix m;
-	m.makeTranslate(10000000.f, 0.f, 0.f);
-	/* alternativ:
-	m.set(1.f, 0.f, 0.f, 0.f,
-	0.f, 1.f, 0.f, 0.f,
-	0.f, 0.f, 1.f, 0.f,
-	10.f, 0.f, 0.f, 1.f);*/
-	lollipop->setMatrix(m);
-	/* alternativ zu MatrixTransform:
-	float theta(3.14*.5f);
-	osg::Vec3 axis(.707f, .707f, 0.f);
-	osg::Quat q0(theta, axis);
-	osg::Vec3 yawAxis(0.f, 0.f, 1.f);
-	osg::Vec3 pitchAxis(1.f, 0.f, 0.f);
-	osg::Vec3 rollAxis(0.f, 1.f, 0.f);
-	osg::Quat q1(0.5f, yawAxis, 0.4f, pitchAxis, 0.3f, rollAxis);
-	q0 *= q1;
-	osg::Vec3 pos(1, 2, 3);
-	osg::Quat att(theta, axis);
-	osg::ref_ptr<osg::PositionAttitudeTransform> pat = new osg::PositionAttitudeTransform;
-	pat->setPosition(pos);
-	pat->setAttitude(att);*/
 
-	osg::Node* logo = osgDB::readNodeFile("fjs.obj");
-	// topologische Struktur
+	// add the geode to the root node
+	//root->addChild(createTriangle());
+	// AUFGABE 3+4
 	root->addChild(addPlane());
-	// lollipop:
-	root->addChild(lollipop);
-	lollipop->addChild(addStick());
-	lollipop->addChild(logo);
+	root->addChild(addStick());
+
+	// AUFGABE 2
+	osgDB::writeNodeFile(*root, "export.osg");
+
+	// AUFGABE 5
+	osg::Node* logo = osgDB::readNodeFile("fjs.obj");
+	root->addChild(logo);
 
 	// create a viewer object to display the scene
 	osgViewer::Viewer viewer;
