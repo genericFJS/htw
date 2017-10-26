9e631d312f68a65f82a530096db179d2fc6babe7
genericFJS <freejailsymphonic@yahoo.de>
2017-10-26 14:23:59 +0200

    Update IT2 Praktikum 1

---
+++ Internet-Technologien 2/Praktikum/prakt1/src/RTSPvlc.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.net.*;
+
+class RTSPvlc {
+
+	public static void main(String argv[]) throws Exception {
+		String[] sentences = new String[7];
+		String modifiedSentence;
+
+		// BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		// sentence = inFromUser.readLine();
+		sentences[0] = "OPTIONS * RTSP/1.0\n";
+		sentences[1] = "DESCRIBE rtsp://idefix.informatik.htw-dresden.de:5554/mystream RTSP/1.0\n";
+		sentences[2] = "SETUP rtsp://idefix.informatik.htw-dresden.de:5554/mystream RTSP/1.0\n";
+		sentences[3] = "PLAY rtsp://idefix.informatik.htw-dresden.de:5554/mystream RTSP/1.0\n";
+		sentences[4] = "PAUSE rtsp://idefix.informatik.htw-dresden.de:5554/mystream RTSP/1.0\n";
+		sentences[5] = "PLAY rtsp://idefix.informatik.htw-dresden.de:5554/mystream RTSP/1.0\n";
+		sentences[6] = "TEARDOWN rtsp://idefix.informatik.htw-dresden.de:5554/mystream RTSP/1.0\n";
+
+		Socket clientSocket = new Socket("idefix.informatik.htw-dresden.de", 5554);
+
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+		for (String sentence : sentences) {
+			System.out.println("SENDING: \n--------\n" + sentence);
+			outToServer.writeBytes(sentence + '\n');
+
+			System.out.println("RECIEVING: \n----------");
+			while ((modifiedSentence = inFromServer.readLine()) != null && (modifiedSentence.length() > 0)) {
+				System.out.println(modifiedSentence);
+			}
+			System.out.println("===========================================================");
+		}
+
+		clientSocket.close();
+
+	}
+}
